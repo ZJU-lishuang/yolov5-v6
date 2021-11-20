16d40577f448cac59d7d0f2afae3dd89a9513234
ls <qqlishuang@gmail.com>
2021-11-20 11:57:50 +0800

    upload train sparsity shell

---
+++ train_sparsity.sh
@@ -0,0 +1,3 @@
+#python train_sparsity.py --img 416 --batch 8 --epochs 50 --data data/coco_hand.yaml --cfg models/yolov5s.yaml --weights runs/train/s_hand/weights/last.pt --name s_hand_sparsity -sr --s 0.001 --prune 1
+
+python train_sparsity.py --img 416 --batch 8 --epochs 50 --data data/coco128.yaml --cfg models/yolov5s.yaml --weights weights/yolov5s.pt --name s_sparsity -sr --s 0.001 --prune 1
